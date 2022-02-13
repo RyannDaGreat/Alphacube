cb2ed648a86cdc6b148c61f86440a5662d233dfa
Ryan Burgert <ryancentralorg@gmail.com>
2022-02-13 01:33:02 -0500

    Succesful resolution increase test! Simply continued training from v0.0.10 into higher resolution.

--- translator/scripts/train_alphablock_without_ssim_256.sh
+++ translator/scripts/train_alphablock_without_ssim_256.sh
@@ -1,1 +1,1 @@
-python3 train.py --config configs/alphablock_without_ssim_256.yaml --output_path trained_models
+python3 train.py --config configs/alphablock_without_ssim_256.yaml --output_path trained_models --resume
